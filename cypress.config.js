79f7ffe70f86dd4ba9df70901d0c3477bde70df0
VladyslavBrchk <shpovalenko.vladyslav@gmail.com>
2023-11-07 15:24:57 +0200

    cypress.config,js update commit

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://demo.realworld.io/#/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
